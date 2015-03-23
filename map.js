300a5bd91edf64d2fd5a7046b79d8dc20b4c6c76
geh4y8 <guy.halperin88@gmail.com>
2015-03-22 17:24:10 -0700

    add radius for aiports within radius and warning message

--- map.js
+++ map.js
@@ -1,23 +1,31 @@
 var coords;
 
+var airports = false;
+
 $.get("http://ipinfo.io", function(response) {
     if(response){
       coords = response.loc.split(",");
-      initial_location(coords[0], coords[1]);
+      check_for_airports(coords[0], coords[1]);
     }
 
 } , "jsonp");
 
 
+function check_for_airports(lat, lng){
 
-
-function initial_location(){
-
-  $.get("http://api.geonames.org/findNearby?lat=" + coords[0] +"&lng=" + coords[1] +"&fcode=AIRP&radius=35&maxRows=100&username=geh4y8", function (response){
+  $.get("http://api.geonames.org/findNearby?lat=" + lat +"&lng=" + lng +"&fcode=AIRP&radius=5&maxRows=100&username=geh4y8", function (response){
     if(response){
-      console.log(response);
+      airports = true;
+      initial_location(lat, lng, response);
+      console.log('airports found')
+    } else{
+      initial_location(lat, lng);
     }
   }, "xml");
+}
+
+
+function initial_location(lat, lng, airports){
 
   L.mapbox.accessToken = 'pk.eyJ1IjoiZ2VoNHk4IiwiYSI6ImFZWVY1WEkifQ.yhJcrdrARJI1Zu0K3nWWvQ';
 
@@ -25,14 +33,46 @@ function initial_location(){
       attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
   });
 
-  var map = L.map('map').addLayer(mapboxTiles).setView([coords[0], coords[1]], 13);
+  var map = L.map('map').addLayer(mapboxTiles).setView([lat, lng], 10);
+  if(airports){
+    var ports = airports.getElementsByTagName("geoname");
+    console.log(ports);
+    for (var i = 0; i<ports.length; i++){
+      var port = ports[i];
+      var portLat = port.children[2].innerHTML;
+      var portLng = port.children[3].innerHTML;
+      L.circle([portLat, portLng], 8000, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5
+      }).addTo(map);
+      L.marker([portLat, portLng]).addTo(map).bindPopup(port.children[1].innerHTML);
 
-  var marker = L.marker([coords[0], coords[1]]).addTo(map);
+    }
 
-  var circle = L.circle([51.508, -0.11], 500, {
-      color: 'red',
-      fillColor: '#f03',
-      fillOpacity: 0.5
-  }).addTo(map);
+      swal({
+        title: 'no fly zone',
+        text: 'You cannot fly here, ' + ports[0].children[1].innerHTML + ' is within a five mile radius!',
+        type: 'warning'
+      });
 
+  }
+
+
+
+
+
+  // function create_airport_marker(airportlat, airportlng){
+  //   var marker = L.marker([airportlat, airportlng]).addTo(map);
+  // }
 }
+
+// function create_airport_marker(lat, lng){
+//   var marker = L.marker([lat, lng]).addTo(map);
+
+//   var circle = L.circle([51.508, -0.11], 500, {
+//       color: 'red',
+//       fillColor: '#f03',
+//       fillOpacity: 0.5
+//   }).addTo(map);
+// }
